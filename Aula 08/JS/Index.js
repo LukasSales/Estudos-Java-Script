59cde7a7eb75b27b1b06e22e7fb29ae7e7c28db2
Lucas Sales <95869098+LukasSales@users.noreply.github.com>
2022-06-05 17:05:57 -0300

    att

---
+++ Aula 08/JS/Index.js
@@ -0,0 +1,3 @@
+var vel = 60
+var velmax = 80
+if (vel>velmax) {console.log(`Você foi multado por estar a ${vel}Km/h em uma via que a velocidade máxima é ${velmax}Km/h`)} else {console.log(`Sua velocidade ${vel}Km/h está dentro do permitido`)} 
